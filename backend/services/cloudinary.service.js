d1d7db6ccc89dad21c1eac5eea2191fc08b3d2cd
Khawar Khan <ma3533899@gmail.com>
2025-04-22 11:35:26 +0500

    Updated multer and cloudinary service

--- backend/services/cloudinary.service.js
+++ backend/services/cloudinary.service.js
@@ -1,42 +1,92 @@
-import { v2 as Cloudinary } from 'cloudinary';
-import fs from 'fs';
+// import { v2 as Cloudinary } from 'cloudinary';
+// import fs from 'fs';
 
-//cofiguring cloudinary service
-Cloudinary.config(
-    {
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-      api_key: process.env.CLOUDINARY_API_KEY, 
-      api_secret: process.env.CLOUDINARY_API_SECRET
-    }
-);
-// function for uploading on cloudinary.
-const uploadOnCloudinary = async (localFilePath) => {
-    try {
-      //if no local file path provided then simpley return null.
-      if (!localFilePath) return null;
+// //cofiguring cloudinary service
+// Cloudinary.config(
+//     {
+//       cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//       api_key: process.env.CLOUDINARY_API_KEY, 
+//       api_secret: process.env.CLOUDINARY_API_SECRET
+//     }
+// );
+// // function for uploading on cloudinary.
+// const uploadOnCloudinary = async (localFilePath) => {
+//     try {
+//       //if no local file path provided then simpley return null.
+//       if (!localFilePath) return null;
   
-      //uploading file on CLoudinary
-      const response = await Cloudinary.uploader.upload(
-        localFilePath,
-        //providing upload options as second parameter.
-        {
-          resource_type: "auto"
-        }
-      );
+//       //uploading file on CLoudinary
+//       const response = await Cloudinary.uploader.upload(
+//         localFilePath,
+//         //providing upload options as second parameter.
+//         {
+//           resource_type: "auto"
+//         }
+//       );
   
-      //file upload successfully.
-      console.log("file Uploaded successfully on Cloudinary");
-      console.log("File is uploaded and the url is : ", response.url);
-      console.log("The response object is : ", response);
+//       //file upload successfully.
+//       console.log("file Uploaded successfully on Cloudinary");
+//       console.log("File is uploaded and the url is : ", response.url);
+//       console.log("The response object is : ", response);
   
-      return response;
+//       return response;
     
-    } catch (error) {
-      fs.unlinkSync(localFilePath); // removes the locally saved file present at local file path for upload on cloudinary, as teh operation failed.
+//     } catch (error) {
+//       fs.unlinkSync(localFilePath); // removes the locally saved file present at local file path for upload on cloudinary, as teh operation failed.
   
-      return null;
+//       return null;
   
-    }
-  };
+//     }
+//   };
   
-export { uploadOnCloudinary };
+// export { uploadOnCloudinary };
+
+// ES6 import
+// import { v2 as Cloudinary } from "cloudinary"; // used ALIAS for v2
+
+// its common JS equivalent
+// const Cloudinary = require('cloudinary').v2;
+import { v2 as Cloudinary} from 'cloudinary';
+import fs from 'fs';
+// const fs = require("fs");
+   
+//configuring cloudinary service.
+Cloudinary.config(
+  {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  }
+);
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    //if no local file path provided then simpley return null.
+    if (!localFilePath) return null;
+
+    //uploading file on CLoudinary
+    const response = await Cloudinary.uploader.upload(
+      localFilePath,
+      //providing upload options as second parameter.
+      {
+        resource_type: "auto"
+      }
+    );
+
+    //file upload successfully.
+    console.log("file Uploaded successfully on Cloudinary");
+    console.log("File is uploaded and the url is : ", response.url);
+    console.log("The response object is : ", response);
+
+    fs.unlinkSync(localFilePath);
+
+    return response;
+  
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // removes the locally saved file present at local file path for upload on cloudinary, as teh operation failed.
+
+    return null;
+
+  }
+};
+export default uploadOnCloudinary;
