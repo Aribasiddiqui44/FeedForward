17c4c750b7d9b60dd571361d84ee83c85454e3c5
Khawar Khan <ma3533899@gmail.com>
2025-04-26 11:35:54 +0500

    added Claimed Donation model

--- backend/models/claimedDonation.model.js
+++ backend/models/claimedDonation.model.js
@@ -4,6 +4,37 @@ import { Donor } from "./donor.model.js";
 import { Donation } from "./donation.model.js";
 import { Rider } from "./rider.model.js";
 const claimedDonationSchema = new Schema({
-
+    donation: {
+        id: {
+            type: Schema.Types.ObjectId,
+            ref: 'Donation'
+        },
+        foodItemName: { type: String },
+        foodItemQuantity: { type: String }
+    },
+    donorId: {
+        id: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Donor'
+        },
+        orgName: { type: String },
+        orgEmail: { type: String }
+    },
+    receiver: {
+        id: {
+            type: Schema.Types.ObjectId,
+        ref: 'Receiver'
+        },
+        orgName: {type: String},
+        orgEmail: {type: String}
+    },
+    volunteer: {
+            id: {
+                type: Schema.Types.ObjectId,
+                ref: 'Rider'
+            },
+            name: { type: String},
+            contact: {type: String}
+    }
 });
 export const ClaimedDonation = mongoose.model('ClaimedDonation', claimedDonationSchema);
