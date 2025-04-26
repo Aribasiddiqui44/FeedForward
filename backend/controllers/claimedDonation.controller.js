74f1296e42912083609b2efb9c5e1a92e10949d4
Khawar Khan <ma3533899@gmail.com>
2025-04-26 11:40:33 +0500

    Initialized claimed donation controller

---
+++ backend/controllers/claimedDonation.controller.js
@@ -0,0 +1,12 @@
+import { ClaimedDonation } from "../models/claimedDonation.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+
+const getAllClaimedDonations = asyncHandler( async(req, res) => {
+
+});
+
+export {
+    getAllClaimedDonations
+}
