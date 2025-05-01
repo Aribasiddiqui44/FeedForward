77c6a111864439422f1b5a1ca4fed967f50ad7ea
Khawar Khan <ma3533899@gmail.com>
2025-05-01 15:01:53 +0500

    added patch for receiver org image

--- backend/controllers/receiver.controller.js
+++ backend/controllers/receiver.controller.js
@@ -3,6 +3,7 @@ import { User } from '../models/user.model.js';
 import asyncHandler from '../utils/asyncHandler.js';
 import ApiError from '../utils/ApiError.js';
 import ApiResponse from '../utils/ApiResponse.js';
+import uploadOnCloudinary from '../services/cloudinary.service.js';
 
 const postReceiverForm = asyncHandler( async (req, res) => {
     const { orgName, orgEmail, address, city, country, contactNumber, postalCode, isAggreedToTermsAndConditions } = req.body;
@@ -33,6 +34,35 @@ const postReceiverForm = asyncHandler( async (req, res) => {
     );
 });
 
+const patchOrgImage = asyncHandler( async(req, res) => {
+    const { receiverId } = req.body;
+    let localImagePath = req.files?.ReceiverImage[0]?.path;
+    if( !localImagePath ) {
+        throw new ApiError(400, "Image not found.");
+    };
+    let uplaodedImage = await uploadOnCloudinary(localImagePath);
+    if( !uplaodedImage ) {
+        throw new ApiError(500, "Internal Server in uploading image, kindly try again.")
+    };
+
+    let updatedReceiver = await Receiver.findByIdAndUpdate(
+        receiverId,
+        {
+            $set: {
+                orgImageUrl: uplaodedImage.url
+            }
+        }
+    
+    );
+    if ( !updatedReceiver ) {
+        throw new ApiError(500, "Internal Server Error, in updating image.")
+    };
+    res.status(201).json(
+        201,
+        updatedReceiver,
+        "Success"
+    );
+});
 
 const patchAddNewAddress = asyncHandler( async(req, res) => {
 
--- backend/routes/receiver.router.js
+++ backend/routes/receiver.router.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import { postReceiverForm } from '../controllers/receiver.controller.js';
 import { verifyJWT } from '../middlewares/authentication.middleware.js';
+import upload from '../middlewares/multer.middleware.js';
 
 const router = express.Router();
 
-router.post('/add', verifyJWT, postReceiverForm); 
-
+router.post('/post', verifyJWT, postReceiverForm); 
+router.patch('/orgImage', 
+    verifyJWT,
+    upload.fields([
+        {
+            name: "ReceiverImage",
+            maxCount: 1
+        }
+    ]),
+    
+)
 export default router;
